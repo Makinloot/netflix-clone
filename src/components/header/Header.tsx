ff344a3a29ef81816646dc94b41ec6f1763dcd86
Tornike <itorn9777@gmail.com>
2023-03-03 13:44:06 +0400

    set header background if scrolled down

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,23 @@
 // media & styles
-import './Header.scss'
-import logo from '../../assets/logo.png'
+import "./Header.scss";
+import logo from "../../assets/logo.png";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [showHeader, setShowHeader] = useState<boolean>(false);
+
+  const handleHeader = () => {
+    if (window.scrollY > 100) setShowHeader(true);
+    else setShowHeader(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleHeader);
+    return () => window.removeEventListener("scroll", handleHeader);
+  }, []);
+
   return (
-    <header className='Header'>
+    <header className={showHeader ? "Header active" : "Header"}>
       <div className="container">
         <div className="Header-wrapper">
           <div className="Header-logo">
@@ -13,7 +26,7 @@ const Header = () => {
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
